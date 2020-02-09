aeb7ba1e98bcf7d974ec0ad2f3cf058888d5c6cc
TEC10101 <tyler.corum@gmail.com>
2020-02-09 12:25:30 -0800

    complete

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IUserSettings } from '../data/user-settings.interface';
+import { NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -8,11 +9,11 @@ import { IUserSettings } from '../data/user-settings.interface';
 })
 export class UserSettingsFormComponent implements OnInit {
   originalUserSettings: IUserSettings = {
-    name: 'Milton',
-    emailOffers: true,
-    interfaceStyle: 'dark',
-    subscriptionType: 'Annual',
-    notes: 'Some Notes'
+    name: null,
+    emailOffers: null,
+    interfaceStyle: null,
+    subscriptionType: null,
+    notes: null
   }
 
   userSettings: IUserSettings = { ...this.originalUserSettings };
@@ -20,6 +21,15 @@ export class UserSettingsFormComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm) {
+    console.log('in onSubmit ', form.valid)
+  }
+  onBlur(field: NgModel) {
+    console.log('in onBlur: ', field.valid)
+    
+    
   }
 
 }
